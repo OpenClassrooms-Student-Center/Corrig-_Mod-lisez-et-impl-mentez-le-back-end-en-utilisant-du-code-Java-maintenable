736242b7af51db1fbb8c9102e29ccc7a23c6e495
Gregoire Cattan <gregoire.cattan@ibm.com>
2025-08-15 20:01:08 +0200

    remove some tests

--- src/test/java/com/example/demo/Projet3BackEndApplicationTests.java
+++ src/test/java/com/example/demo/Projet3BackEndApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Projet3BackEndApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
